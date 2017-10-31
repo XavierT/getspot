e6634e63e745950ac75c48ff546ec04a33a7dcb0
XavierT <sseingalt@gmail.com>
2017-10-31 19:44:33 +0100

    add basic clap method to manage CLI options

--- src/main.rs
+++ src/main.rs
@@ -5,10 +5,34 @@ extern crate simple_logger;
 extern crate clap;
 
 
+use clap::{Arg, App};
+
 fn main() {
     simple_logger::init().unwrap();
 
+  let matches = App::new("getspot")
+                          .version("0.1")
+                          .author("Xavier T. <sseingalt@gmail.com>")
+                          .about("Copy pictures from Spotlight directory")
+                          .arg(Arg::with_name("target")
+                               .short("t")
+                               .long("target")
+                               .value_name("PATH")
+                               .help("Sets output picture directory")
+                               .takes_value(true))
+                          .arg(Arg::with_name("v")
+                               .short("v")
+                               .multiple(true)
+                               .help("Sets the level of verbosity"))
+                          .get_matches();
+
+    // Gets a value for config if supplied by user, or defaults to "default.conf"
+    let config = matches.value_of("target").unwrap_or("default.conf");
+    println!("Value for config: {}", config);
+
+
     info!("This is an example message.");
+
 }
 
 // pub enum LogLevel {
