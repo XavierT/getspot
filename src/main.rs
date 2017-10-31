c3f81d45f1d1982b58ee18dc7ff44c533344efc6
XavierT <sseingalt@gmail.com>
2017-10-31 20:54:56 +0100

    Format file using rustfmt

--- src/main.rs
+++ src/main.rs
@@ -10,43 +10,44 @@ use log::LogLevel;
 
 fn main() {
 
-  let matches = App::new("getspot")
-                          .version("0.1")
-                          .author("Xavier T. <sseingalt@gmail.com>")
-                          .about("Copy pictures from Spotlight directory")
-                          .arg(Arg::with_name("target")
-                               .short("t")
-                               .long("target")
-                               .value_name("PATH")
-                               .help("Sets output picture directory")
-                               .takes_value(true))
-                          .arg(Arg::with_name("v")
-                               .short("v")
-                               .multiple(true)
-                               .help("Sets the level of verbosity, from -v to -vvv"))
-                          .get_matches();
+    let matches = App::new("getspot")
+        .version("0.1")
+        .author("Xavier T. <sseingalt@gmail.com>")
+        .about("Copy pictures from Spotlight directory")
+        .arg(
+            Arg::with_name("target")
+                .short("t")
+                .long("target")
+                .value_name("PATH")
+                .help("Sets output picture directory")
+                .takes_value(true),
+        )
+        .arg(Arg::with_name("v").short("v").multiple(true).help(
+            "Sets the level of verbosity, from -v to -vvv",
+        ))
+        .get_matches();
 
     // Gets a value for config if supplied by user, or defaults to "default.conf"
     let config = matches.value_of("target").unwrap_or("default.conf");
     println!("Value for config: {}", config);
 
-// pub enum LogLevel {
-//     Error,
-//     Warn,
-//     Info,
-//     Debug,
-//     Trace,
-// }   
+    // pub enum LogLevel {
+    //     Error,
+    //     Warn,
+    //     Info,
+    //     Debug,
+    //     Trace,
+    // }
 
     let level;
 
     match matches.occurrences_of("v") {
-        0 => { level = LogLevel::Error},
-        1 => { level = LogLevel::Warn},
-        2 => { level = LogLevel::Info},
-        3 | _ => { level = LogLevel::Trace},
+        0 => level = LogLevel::Error,
+        1 => level = LogLevel::Warn,
+        2 => level = LogLevel::Info,
+        3 | _ => level = LogLevel::Trace,
     }
-     
+
     simple_logger::init_with_level(level).unwrap();
 
     info!("This is an example message.");
@@ -54,4 +55,3 @@ fn main() {
     error!("This is an example message.");
 
 }
-
