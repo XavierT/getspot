10b6ee1bbca18e859d2e49417be8d13570e43d17
XavierT <sseingalt@gmail.com>
2017-10-31 19:35:36 +0100

    add logging crate

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,20 @@
+
+#[macro_use]
+extern crate log;
+extern crate simple_logger;
+extern crate clap;
+
+
 fn main() {
-    println!("Hello, world!");
+    simple_logger::init().unwrap();
+
+    info!("This is an example message.");
 }
+
+// pub enum LogLevel {
+//     Error,
+//     Warn,
+//     Info,
+//     Debug,
+//     Trace,
+// }
