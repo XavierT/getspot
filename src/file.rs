57e56731fb47254712dacce26a9880ae11bbd236
XavierT <sseingalt@gmail.com>
2017-11-01 14:07:12 +0100

    add some tests

--- src/file.rs
+++ src/file.rs
@@ -11,6 +11,7 @@ pub struct PictureFile {
 }
 
 impl PictureFile {
+    /// Default empty picture file
     pub fn new() -> PictureFile {
         PictureFile {
             path: None,
@@ -19,7 +20,52 @@ impl PictureFile {
             height: None,
         }
     }
-    // add code here
 
-    
+    /// Add dimensions to picture file
+    pub fn with_dimensions(mut self, w:u32, h:u32) ->  PictureFile{
+        self.width = Some(w);
+        self.height = Some(h);
+        self
+    }
+
+    /// Add title to picture file
+    pub fn with_title( mut self, t: &str) -> PictureFile{
+        self.title = Some(String::from(t));
+        self
+    }
+
+    /// Return a string of the dimension of the file
+    /// If dimension are not defined, return "default"
+    //
+    // This is to be used to copy file in appropriate folder
+    // depending on their dimensions
+    pub fn get_dimension_string(&self) -> String{
+
+        match (self.width, self.height) {
+            (Some(w), Some(h)) => format!("{}x{}",w, h),
+            _ => String::from("default"),
+        }
+        
+    }
+
 }
+
+#[cfg(test)]
+mod tests {
+
+    use ::file::{PictureFile};
+
+    #[test]
+    fn test_dimension_string() {
+        let pic1 = PictureFile::new();
+        assert_eq!(pic1.get_dimension_string(), "default" );
+
+        let pic2 = PictureFile::new().with_dimensions(1280,1024);
+        assert_eq!(pic2.get_dimension_string(),"1280x1024");
+        assert_ne!(pic2.get_dimension_string(),"128x102");
+
+        let pic3 = PictureFile::new().with_dimensions(640,480);
+        assert_eq!(pic3.get_dimension_string(),"640x480");
+    }
+}
+    
