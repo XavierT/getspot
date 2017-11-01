0bb258c3527765d8e59cf66c9e716cf5589cfe7a
XavierT <sseingalt@gmail.com>
2017-11-01 15:09:43 +0100

    run rustfmt

--- src/file.rs
+++ src/file.rs
@@ -22,20 +22,20 @@ impl PictureFile {
     }
 
     /// Add source path to picture
-    pub fn with_path( mut self, p: PathBuf) -> PictureFile{
+    pub fn with_path(mut self, p: PathBuf) -> PictureFile {
         self.path = Some(p);
         self
     }
 
     /// Add dimensions to picture file
-    pub fn with_dimensions(mut self, w:u32, h:u32) ->  PictureFile{
+    pub fn with_dimensions(mut self, w: u32, h: u32) -> PictureFile {
         self.width = Some(w);
         self.height = Some(h);
         self
     }
 
     /// Add title to picture
-    pub fn with_title( mut self, t: &str) -> PictureFile{
+    pub fn with_title(mut self, t: &str) -> PictureFile {
         self.title = Some(String::from(t));
         self
     }
@@ -45,33 +45,31 @@ impl PictureFile {
     //
     // This is to be used to copy file in appropriate folder
     // depending on their dimensions
-    pub fn get_dimension_string(&self) -> String{
+    pub fn get_dimension_string(&self) -> String {
 
         match (self.width, self.height) {
-            (Some(w), Some(h)) => format!("{}x{}",w, h),
+            (Some(w), Some(h)) => format!("{}x{}", w, h),
             _ => String::from("default"),
         }
-        
-    }
 
+    }
 }
 
 #[cfg(test)]
 mod tests {
 
-    use ::file::{PictureFile};
+    use file::PictureFile;
 
     #[test]
     fn test_dimension_string() {
         let pic1 = PictureFile::new();
-        assert_eq!(pic1.get_dimension_string(), "default" );
+        assert_eq!(pic1.get_dimension_string(), "default");
 
-        let pic2 = PictureFile::new().with_dimensions(1280,1024);
-        assert_eq!(pic2.get_dimension_string(),"1280x1024");
-        assert_ne!(pic2.get_dimension_string(),"128x102");
+        let pic2 = PictureFile::new().with_dimensions(1280, 1024);
+        assert_eq!(pic2.get_dimension_string(), "1280x1024");
+        assert_ne!(pic2.get_dimension_string(), "128x102");
 
-        let pic3 = PictureFile::new().with_dimensions(640,480);
-        assert_eq!(pic3.get_dimension_string(),"640x480");
+        let pic3 = PictureFile::new().with_dimensions(640, 480);
+        assert_eq!(pic3.get_dimension_string(), "640x480");
     }
 }
-    
