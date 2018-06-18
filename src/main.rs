85e3b3a383cd149422afd37efd25669f1492d167
XavierT <sseingalt@gmail.com>
2018-06-18 21:38:23 +0200

    remove useless trace

--- src/main.rs
+++ src/main.rs
@@ -45,8 +45,8 @@ fn main() {
         .get_matches();
 
     // Gets a value for config if supplied by user, or defaults to "default.conf"
-    let config = matches.value_of("target").unwrap_or("default.conf");
-    println!("Value for config: {}", config);
+    // let config = matches.value_of("target").unwrap_or("default.conf");
+    // println!("Value for config: {}", config);
 
     // pub enum LogLevel {
     //     Error,
